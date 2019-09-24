ffbcb6f27e49e171ad2a34fe1ecb56e76eda5693
unknown <sabdullaeef87@gmail.com>
2019-09-24 11:31:07 +0100

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Router, Link } from "react-router-dom";
+import Navigation from "./Components/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <Route path="/Navigation" component={Navigation} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
